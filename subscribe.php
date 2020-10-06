658729cc1d4531b8e476111f131622429889847d
Timothy7310 <penlist127@gmail.com>
2020-10-06 14:33:10 +0300

    fixed: phpmailer & packages BEM

--- subscribe.php
+++ subscribe.php
@@ -5,18 +5,12 @@ require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
 
 // Переменные, которые отправляет пользователь
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
 $email = $_POST['email'];
 
 // Формирование самого письма
 $title = "Новое обращение Best Tour Plan";
 $body = "
 <h2>Новое обращение</h2>
-<b>Имя:</b> $name<br>
-<b>Телефон:</b> $phone<br><br>
-<b>Сообщение:</b><br>$message
 <b>Почта:</b> $email<br>
 ";
 
@@ -56,6 +50,7 @@ else {$result = "error";}
 }
 
 // Отображение результата
-header('Location: thankyou.html');
+header('Location: subscribe.html');
 ?>
+
 
